ba35625ff4cc41f57612d086242a5fb065b10c65
Syed Tabish Babar Zaidi <tabishzaidi.ai@gmail.com>
2026-02-03 21:32:22 +0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -8,9 +8,12 @@ export default defineConfig(({ mode }) => {
       server: {
   host: '0.0.0.0',
   port: 5173,
-  preview: {
-    allowedHosts: ['amrahai-production.up.railway.app']
-  }
+      preview: {
+      host: '0.0.0.0',
+      port: 4173,
+      allowedHosts: ['amrahai-production.up.railway.app']
+    }
+
 }
       plugins: [react()],
       define: {
