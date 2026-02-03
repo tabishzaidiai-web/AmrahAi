74b101a1a79f74955df18279e1286dc01c89e70a
Syed Tabish Babar Zaidi <tabishzaidi.ai@gmail.com>
2026-02-03 22:34:29 +0400

    Add allowedHosts to server config to fix blocked request

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ export default defineConfig(({ mode }) => {
       server: {
   host: '0.0.0.0',
   port: 5173,
+                allowedHosts: ['amrahai-production.up.railway.app'],
       preview: {
       host: '0.0.0.0',
       port: 4173,
